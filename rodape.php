2cf8785c525fa9955baf27ceb082bc0af6fbca0d
etevaldojales <etevaldojales@gmail.com>
2026-07-17 09:42:17 -0300

    Atualização e melhorias complemento3

--- rodape.php
+++ rodape.php
@@ -1,3 +1,27 @@
+<?php
+$nome_loja = "";
+$ano = date("Y");
+if (file_exists("config_inicio.php")) {
+	require_once("config_inicio.php");
+	require_once($lib . 'classes/class.empresa.php');
+	$_class = new empresa($dbase);
+	$emp = $_class->get(1);
+
+	if (!empty($emp['nome'])) {
+		$nome_loja = htmlspecialchars($emp['nome']);
+	}
+
+	if (isset($emp['ano_fundacao'])) {
+		$ano = date("Y") . " - " . $emp['ano_fundacao'];
+	}
+}
+
+if (empty($nome_loja)) {
+	$nome_loja = "LOJA - DEMO";
+}
+?>
+
 <div id="footer">
-	LOJA - DEMO<br>© Copyright Jales Tecnologia
+	<?php echo $nome_loja; ?><br>© Copyright
+	<?php echo $ano; ?> Jales Tecnologia
 </div>
--- titulo.php
+++ titulo.php
@@ -1,1 +1,21 @@
-<title>LOJA - DEMO</title>
+<?php
+$nome_loja = "";
+if (file_exists("config_inicio.php")) {
+    require_once("config_inicio.php");
+    require_once($lib . 'classes/class.empresa.php');
+    $_class = new empresa($dbase);
+    $emp = $_class->get(1);
+
+    if (!empty($emp['nome'])) {
+        $nome_loja = htmlspecialchars($emp['nome']);
+    }
+}
+
+if (empty($nome_loja)) {
+    $nome_loja = "LOJA - DEMO";
+}
+?>
+
+<title>
+    <?php echo $nome_loja; ?>
+</title>
